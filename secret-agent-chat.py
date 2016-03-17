80dae394d2cf12eae411acfb7dffd6894a0f1896
David Lee <David Lee>
2016-03-17 15:12:15 +0900

    Update secret-agent-chat

--- secret-agent-chat.py
+++ secret-agent-chat.py
@@ -3,3 +3,5 @@ from random import randint
 ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
 def generate_opt (sheets, length):
+	for sheet in range(sheets):
+		with open ("opt" + str(sheet) + ".txt","w") as f:
